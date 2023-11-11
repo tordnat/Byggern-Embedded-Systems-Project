45dd785eed48f873931a45255ee823852aa83550
tordnat <tordna@protonmail.com>
2023-11-11 11:10:01 +0100

    Add: Working OpenCV contour

---
+++ node3/build/CMakeFiles/simple-demo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple-demo.dir/ball_tracking.cpp.o"
+  "CMakeFiles/simple-demo.dir/ball_tracking.cpp.o.d"
+  "CMakeFiles/simple-demo.dir/marker_detection.cpp.o"
+  "CMakeFiles/simple-demo.dir/marker_detection.cpp.o.d"
+  "simple-demo"
+  "simple-demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
